bee98d6c21c339e808219f5010a637d9ddc140c7
“CreamToby” <hyson09@gmail.com>
2022-02-28 16:31:56 -0500

    JSBasicsExercise

---
+++ JSBasicsExercise/script.js
@@ -0,0 +1,60 @@
+// Number 1. favorite number
+console.log(3);
+
+// Number 2. 5 main data types
+// 1. Numbers (0; 7; -4;, 0.7423;)
+// 2. Strings ("Heeyoung Son";)
+// 3. Booleans (true; false;)
+// 4. Null (null;)
+// 5. Undefined (undefined;)
+
+// Number 3. 6 operator symbols
+// 1. adding (1+8;)
+// 2. subtracting (50-2;)
+// 3. multiplying (2*6;)
+// 4. dividing (9/3;)
+// 5. exponentiation (2**4;)
+// 6. modulus (15%4;)
+
+// Number 4
+console.log(5%2);
+console.log(5%4);
+
+// Number 5
+console.log("I'M MARY POPPINS, Y'ALL!");
+
+// Number 6
+console.log("Hawk "+"eye");
+
+// Number 7
+console.log("Heeyoung Son".length);
+
+// Number 8
+console.log("Avengers: Endgame"[16]);
+
+// Bonus
+// 1
+console.log("\\");
+
+// 2
+console.log(5**4-9);
+
+// 3
+console.log(("I".length + "AM".length) * "Iron".length % "Man".length);
+
+// 4
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+// 5
+var m = 25;
+var c = 4;
+var u = m + c;
+console.log(u);
+
