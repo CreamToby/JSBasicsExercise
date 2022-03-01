b02928ff7e79306cd63ba7e0c3406eee351019f3
“CreamToby” <hyson09@gmail.com>
2022-03-01 10:18:11 -0500

    all answers

--- JSBasicsExercise/script.js
+++ JSBasicsExercise/script.js
@@ -22,15 +22,21 @@ console.log(5%4);
 
 // Number 5
 console.log("I'M MARY POPPINS, Y'ALL!");
+// console.log('I\'M MARY POPPINS, Y\'ALL!');
 
 // Number 6
 console.log("Hawk"+"eye");
 
 // Number 7
 console.log("Heeyoung Son".length);
+// console.log("Heeyoung".length + "Son".length);
 
 // Number 8
 console.log("Avengers: Endgame"[16]);
+
+// Breakdown
+// console.log("Avengers: Endgame".length);
+// console.log("Avengers: Endgame"[16]); have to -1 ! index starts 0
 
 // Bonus
 // 1
@@ -41,6 +47,8 @@ console.log(5**4-9);
 
 // 3
 console.log(("I".length + "AM".length) * "Iron".length % "Man".length);
+// console.log("I".length + "AM".length % "Iron".length - "Man".length);
+// console.log(("I".length - "AM".length) + ("Iron".length - "Man".length));
 
 // 4
 console.log("CAP"[1]);
@@ -51,10 +59,22 @@ console.log("GROOT"[0]);
 console.log("DOCTOR STRANGE"[13]);
 console.log("THOR"[3]);
 console.log("STAR-LORD"[0]);
+// OR
+console.log("CAP"[1] + "VISION"[0] + "OKOYE"[4] + "NICK FURY"[0] + "GROOT"[0] + "DOCTOR STRANGE"[13] + "THOR"[3] + "STAR-LORD"[0]);
+// OR
+lArray = ["CAP", "VISION", "OKOYE", "NICK FURY", "GROOT", "DOCTOR STRANGE", "THOR", "STAR-LORD"];
+console.log(lArray [0][1] + lArray [1][0] + lArray[2][4] + lArray[3][0] + lArray[4][0] + lArray[5][13] + lArray[6][3] + lArray[7][0]);
 
 // 5
 var m = 25;
 var c = 4;
 var u = m + c;
 console.log(u);
+
+/*
+let m = 25;
+let c = 4;
+let u = m + c;
+console.log(u);
+*/
 
